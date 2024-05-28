854c2026e41cea08ebdef560f02bdb5b14767e77
yonghwanbot <jeildangxp@gmail.com>
2024-05-28 12:47:50 -0700

    ORJ

--- gitadd.sh
+++ gitadd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This bash script adds all files to staging.
+
 # Loop through each item in the current directory
 for item in * .*; do
     # Check if the item is not the DemoDir and not the special directories '.' and '..'
@@ -10,16 +12,16 @@ for item in * .*; do
 done
 
 # Check if the branch exists in the local repository
-if git show-ref --quiet refs/heads/RippleJoin; then
+if git show-ref --quiet refs/heads/ORJ; then
     # Checkout the RippleJoin branch if it exists
-    git checkout RippleJoin
+    git checkout ORJ
 else
     # Create the RippleJoin branch and switch to it
-    git checkout -b RippleJoin
+    git checkout -b ORJ
 fi
 
 # Commit the changes
-git commit -m "Ripple Join test"
+# git commit -m "Ripple Join"
 
 # Push the changes to the RippleJoin branch on the remote
 # git push IDEA -u RippleJoin
